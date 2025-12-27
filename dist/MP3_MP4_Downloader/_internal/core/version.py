71c670272f1841932516e6bbcc7883e98117221c
mattbtr <m.beiterhhh@googlemail.com>
2025-12-27 13:46:16 +0100

    added widnows exe build with action + Remove build and dist folders from version control

--- dist/MP3_MP4_Downloader/_internal/core/version.py
+++
@@ -1,4 +0,0 @@
-# core/version.py
-APP_VERSION = "1.0.0"
-def get_version():
-    return APP_VERSION
--- dist/MP3_MP4_Downloader/_internal/gui/widgets/__init__.py
+++
@@ -1,9 +0,0 @@
-# gui/widgets/__init__.py
-from .url_input import UrlInput
-from .format_selector import FormatSelector
-from .action_buttons import ActionButtons
-from .status_label import StatusLabel
-from .download_progress import DownloadProgress
-from .download_path_selector import DownloadPathSelector
-from .version_label import VersionLabel
-
